c12460d2a5e47187716ef0ffae528957cbb5de41
NguyenPhucHuy <phuchuya218@gmail.com>
2024-10-21 15:46:48 +0700

    Add laptrinhc code files

---
+++ main.c
@@ -0,0 +1,1 @@
+#include <stdio.h>\nextern void shellc1();\nextern void shellc2();\nint main() {\n    shellc1();\n    shellc2();\n    return 0;\n}
---
+++ shellc1.c
@@ -0,0 +1,1 @@
+#include <stdio.h>\nvoid shellc1() {\n    printf("This is shellc1\\n");\n}
